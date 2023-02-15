495eca1bd502bc598e80ec144628e55158d51b0c
Matan Avital <matavital13@gmail.com>
2023-02-15 13:23:07 +0200

    changed .gitignore

--- ext_to_txt.py
+++ ext_to_txt.py
@@ -2,28 +2,32 @@ import os
 import pytesseract
 from PIL import Image
 
-# Path to the folder containing image files
-image_folder = '/home/develeap/Documents/extract_text/photos_to_extract'
+image_folder = './photos_to_extract'
 
-# Variable to hold the combined extracted text from all images
-all_text = ''
+photo_text = ''
 
 # Loop through each image file in the folder
 for filename in os.listdir(image_folder):
     if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.bmp'):
+
         # Open the image file
         img = Image.open(os.path.join(image_folder, filename))
 
         # Extract the text from the image
         image_text = pytesseract.image_to_string(img, config='-c tessedit_create_txt=1')
 
-        # Remove the first two lines of the extracted text
+        # Remove the first 3 lines of the extracted text
         lines = image_text.split('\n')[3:]
+
         image_text = '\n'.join(lines)
 
-        # Append the extracted text to the all_text variable
-        # all_text += f"==== {filename} ====\n{image_text}\n\n"
-        all_text += f"==========================================\n{image_text}\n\n"
-# Write the combined extracted text to a single text file
-with open('all_text.txt', 'w') as f:
-    f.write(all_text)
+        # Remove any blank lines from the extracted text
+        image_text = '\n'.join([line for line in image_text.split('\n') if line.strip()])
+
+        # Append the extracted text to the photo_text variable
+        photo_text += f"==========================================\n{image_text}\n\n"
+        # photo_text += f"==== {filename} ====\n{image_text}\n\n"
+
+
+with open('photo_text.md', 'w') as f:
+    f.write(photo_text)
